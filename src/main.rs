30dfef4924520567b68672260642d386e8c40381
noname <48761044+noname0310@users.noreply.github.com>
2021-07-20 20:03:18 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use ndarray::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let arr = array![
+        [0, 1, 2 ,3],
+        [4, 5, 6, 7],
+        [8, 9, 10, 11]
+    ];
+    println!("{0}",arr.slice(s![0.., 1..]));
+    println!("{0}",arr.slice(s![1.., 1..]));
+    println!("{0}",arr.slice(s![1..2, 1..2]));
 }
