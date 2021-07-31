6826933fd69afa72d92161f6c80734e576a5f4a5
noname <48761044+noname0310@users.noreply.github.com>
2021-08-01 03:32:06 +0900

    Update functions.rs

--- src/functions.rs
+++ src/functions.rs
@@ -3,12 +3,11 @@ use ndarray::Array;
 
 pub fn sigmoid_vec(x: &Vec<f64>) -> Vec<f64> {
     let mut sigmoid = x.clone();
-    for i in 0..sigmoid.len() {
-        sigmoid[i] = 1.0 / (1.0 + ((-x[i]).exp2()));
+    for (s, e) in sigmoid.iter_mut().zip(x) {
+        *s = 1.0 / (1.0 + ((-e).exp2()));
     }
     sigmoid
 }
-
 
 pub fn softmax_vec(x: &Vec<f64>) -> Vec<f64> {
     let mut result = x.clone();
@@ -31,6 +30,14 @@ pub fn softmax_vec(x: &Vec<f64>) -> Vec<f64> {
         result.iter_mut().for_each(|e| *e = e.clone() / sum);
     };
     result
+}
+
+pub fn sigmoid<D: ndarray::Dimension>(x: &Array<f64, D>) -> Array<f64, D> {
+    let mut sigmoid = x.clone();
+    for (s, e) in sigmoid.iter_mut().zip(x) {
+        *s = 1.0 / (1.0 + ((-e).exp2()));
+    }
+    sigmoid
 }
 
 pub fn softmax<D: ndarray::Dimension>(x: &Array<f64, D>) -> Array<f64, D> {
@@ -59,4 +66,25 @@ pub fn softmax<D: ndarray::Dimension>(x: &Array<f64, D>) -> Array<f64, D> {
 pub fn cross_entropy_error<D: ndarray::Dimension>(y: &Array<f64, D>, t: &Array<f64, D>) -> f64 {
     let delta = 1e-7;
     -(t * y.mapv(|x| (x + delta).log10())).sum()
+}
+
+pub fn numerical_gradient<D: ndarray::Dimension>(f, x) -> f64 {
+    let h = 1e-4;
+    let grad = np.zeros_like(x);
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while !it.finished {
+        idx = it.multi_index;
+        tmp_val = x[idx];
+        x[idx] = float(tmp_val) + h;
+        fxh1 = f(x);
+        
+        x[idx] = tmp_val - h;
+        fxh2 = f(x);
+        grad[idx] = (fxh1 - fxh2) / (2*h);
+        
+        x[idx] = tmp_val;
+        it.iternext();
+    }
+    grad
 }
