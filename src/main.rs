e3a1a7a52db4b0bfbeebc9d0ab2ca221bc075fc1
noname <48761044+noname0310@users.noreply.github.com>
2021-08-02 11:27:42 +0900

    add bevy to project

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use plotlib::page::Page;
 use plotlib::repr::Plot;
 use plotlib::view::ContinuousView;
 use plotlib::style::{LineStyle};
+use bevy::prelude::*;
 
 fn make_plot<T: Into<String>>(vec: &Vec<f64>, f: fn(v: &Vec<f64>) -> Vec<f64>, colour: T) -> Plot {
     Plot::new(
@@ -15,6 +16,45 @@ fn make_plot<T: Into<String>>(vec: &Vec<f64>, f: fn(v: &Vec<f64>) -> Vec<f64>, c
     LineStyle::new()
         .colour(colour),
     )
+}
+
+struct Position { x: f32, y: f32 }
+
+fn print_position_system(query: Query<&Transform>) {
+    for transform in query.iter() {
+        println!("position: {:?}", transform.translation);
+    }
+}
+
+struct Entity(u64);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+struct Person;
+
+struct Name(String);
+
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("Elaina Proctor".to_string()));
+    commands.spawn().insert(Person).insert(Name("Renzo Hume".to_string()));
+    commands.spawn().insert(Person).insert(Name("Zayna Nieves".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("hello {}!", name.0);
+    }
+}
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.add_startup_system(add_people.system())
+            .add_system(hello_world.system())
+            .add_system(greet_people.system());
+    }
 }
 
 fn main() {
@@ -31,4 +71,8 @@ fn main() {
         .y_label("y");
 
     Page::single(&v).save("scatter.svg").unwrap();
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
