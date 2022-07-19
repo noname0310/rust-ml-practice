238393cb00c5f4bab31aa86fe8cead4c0a1badc7
noname <48761044+noname0310@users.noreply.github.com>
2022-07-19 11:53:39 +0900

    commit

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,4 @@ fn main() {
         .y_label("y");
 
     Page::single(&v).save("scatter.svg").unwrap();
-    // App::build()
-    //     .add_plugins(DefaultPlugins)
-    //     .add_plugin(HelloPlugin)
-    //     .run();
 }
