664d77568044b8cc6e27999be223ce38562f4c7f
noname <48761044+noname0310@users.noreply.github.com>
2022-07-19 17:20:50 +0900

    Revert "Revert "commit""
    This reverts commit 0d0f59ad6997709e284206d6ed84269ddc486b98.

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,44 @@
 mod functions;
 
-use plotlib::page::Page;
-use plotlib::repr::Plot;
-use plotlib::view::ContinuousView;
-use plotlib::style::{LineStyle};
+use std::ops::Range;
 
-fn make_plot<T: Into<String>>(vec: &Vec<f64>, f: fn(v: &Vec<f64>) -> Vec<f64>, colour: T) -> Plot {
-    Plot::new(
-        vec.iter()
-        .zip(f(&vec))
-        .map(|(x, y)| (x.clone(), y.clone()))
-        .collect()
-    ).line_style(
-    LineStyle::new()
-        .colour(colour),
-    )
+use plotters::{
+    style::colors,
+    prelude::{BitMapBackend, ChartBuilder, IntoDrawingArea, LineSeries}
+};
+
+use ndarray::prelude::*;
+
+fn draw_plot<F: FnMut(f64) -> f64>(
+    f: F,
+    x_range: Range<f64>,
+    y_range: Range<f64>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("plot.png", (640, 480)).into_drawing_area();
+    
+    root.fill(&colors::WHITE)?;
+
+    let mut chart = ChartBuilder::on(&root)
+        .margin(10)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(x_range.clone(), y_range.clone())?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(
+            LineSeries::new(
+                
+                &colors::RED,
+            )
+        )?;
+
+    root.present()?;
+
+    Ok(())
 }
 
 fn main() {
-    let range_start = -80;
-    let range_end = 80;
-    let vec: Vec<f64> = (range_start..range_end).into_iter().map(|x| x as f64 / 10.).collect();
-
-    let v = ContinuousView::new()
-        .add(make_plot(&vec, functions::sigmoid_vec, "#DD3355"))
-        .add(make_plot(&vec, functions::relu_vec, "#55DD33"))
-        .add(make_plot(&vec, functions::step_vec, "#0000AA"))
-        //.add(make_plot(&vec, functions::softmax_vec, "#00AA00"))
-        .x_range((range_start / 10) as f64, (range_end / 10) as f64)
-        .y_range(0., 1.)
-        .x_label("x")
-        .y_label("y");
-
-    Page::single(&v).save("scatter.svg").unwrap();
+    draw_plot(&functions::relu(&Array::from_vec(vec![-1.0, 0.0, 1.0]))).unwrap();
 }
